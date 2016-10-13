c75b19a6d360fb03aa2a7614135899e1af481c91
Jugraj Singh <jugrajskhalsa@gmail.com>
2016-10-13 12:03:45 +0530

    Implementing send action for message

--- app/src/main/java/com/example/jugraj/gndecsms/new_message.java
+++ app/src/main/java/com/example/jugraj/gndecsms/new_message.java
@@ -1,11 +1,18 @@
 package com.example.jugraj.gndecsms;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class new_message extends AppCompatActivity {
 
@@ -17,14 +24,40 @@ public class new_message extends AppCompatActivity {
         final EditText sender = (EditText) findViewById(R.id.sender);
         final EditText receiver = (EditText) findViewById(R.id.receiver);
         final EditText message = (EditText) findViewById(R.id.message);
+
         Button sendButton = (Button) findViewById(R.id.send_button);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
                 String from = String.valueOf(sender.getText());
                 String to = String.valueOf(receiver.getText());
                 String sms = String.valueOf(message.getText());
-                // send message
+                RequestQueue queue = Volley.newRequestQueue(v.getContext());
+                String url = "https://www.google.com";
+                StringRequest request = new StringRequest(Request.Method.GET,
+                        url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Snackbar.make(v, response, Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Snackbar.make(v, error.toString(), Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                });
+                queue.add(request);
+
+            }
+        });
+
+        Button cancelButton = (Button) findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
             }
         });
     }
